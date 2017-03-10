cc750dea1b7deef5c7bb16b2abcc84eb3b433d2c
Vassilina Movko <vassilinamvk@gmail.com>
2017-03-10 20:07:40 +0200

    taks prototype (1.part)

--- Project/assets/js/timer.js
+++ Project/assets/js/timer.js
@@ -1,65 +1,35 @@
-    var _second = 1000;
-    var _minute = _second * 60;
-    var _hour = _minute * 60;
-    var _day = _hour * 24;
-	
+	var _seconde = 1000;
+	var _minute = _seconde * 60;
+	var _hour = _minute * 60;
+	var _day = _hour * 24;
 
-    function showRemaining(end, place, timer) {
-	
-        var now = new Date();
-        var distance = end - now;
-        if (distance < 0) {
+	function showRemaining(end) {
+		var now = new Date();
+		var distance = end - now;
+		if (distance < 0) {
 
-            clearInterval(timer);
-            document.getElementById(place).innerHTML = "Aeg on l&auml;bi!";
+			clearInterval(timer);
+			document.getElementById('counter').innerHTML = "Time is up!";
 
-            return;
-
-        } else if (place  == "countdown") {
-
-		if (distance < 600000) {
-
-			document.getElementById(place).style.color = "#ff0000";
-
+			return;
 		}
-
-	} else if (distance < 86400000) {
-
-		document.getElementById(place).style.color = "#ff0000";
-
-	}
-	
-        var days = Math.floor(distance / _day);
-        var hours = Math.floor((distance % _day) / _hour);
-        var minutes = Math.floor((distance % _hour) / _minute);
-        var seconds = Math.floor((distance % _minute) / _second);
+		var days = Math.floor(distance / _day);
+		var hours = Math.floor((distance % _day) / _hour);
+		var minutes = Math.floor((distance % _hour) / _minute);
+		var seconds = Math.floor((distance % _minute) / _seconde);
+		if (hours   < 10) {hours   = "0"+hours;}
 		if (minutes < 10) {minutes = "0"+minutes;}
 		if (seconds < 10) {seconds = "0"+seconds;}
 
-		if ( days == 1 ){
-
-			document.getElementById(place).innerHTML = days + " päev ";
-
-		} else if ( days > 1 ){
-
-			document.getElementById(place).innerHTML = days + " päeva ";
-
+		if (days == 1){
+			document.getElementById('counter').innerHTML = days + " day ";
+			document.getElementById('counter').innerHTML += hours + ":";
+		} else if (days > 1){
+			document.getElementById('counter').innerHTML = days + " days ";
+			document.getElementById('counter').innerHTML += hours + ":";
 		} else {
-
-			document.getElementById(place).innerHTML = "";
-
+			document.getElementById('counter').innerHTML = hours + ":";
 		}
-
-		if( hours == 1 ){
-
-				document.getElementById(place).innerHTML += hours + " tund ";
-
-			} else if( hours > 1 || hours == 0 ){
-
-				document.getElementById(place).innerHTML += hours + " tundi ";
-
-			} 	
-
-			document.getElementById(place).innerHTML += minutes + ":";
-			document.getElementById(place).innerHTML += seconds;
+			document.getElementById('counter').innerHTML += minutes + ":";
+			document.getElementById('counter').innerHTML += seconds;
 	}
